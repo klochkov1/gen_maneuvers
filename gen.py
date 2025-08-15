cdb90af4a963033d7f95ca7e2b2e987813c8570a
Oleksandr Klochkov <klochkov1w@gmail.com>
2025-08-15 11:52:40 +0300

    change params

--- gen.py
+++ gen.py
@@ -22,8 +22,8 @@ y = random.choice([-1, 1]) * random.randint(2000, 5000)
 heading = random.randint(0, 360) 
 
 # Base flight params
-speed_min = 40
-speed_max = 60
+speed_min = 38
+speed_max = 62
 # basic_flight_duration_min = 10
 basic_flight_duration_min = 5
 basic_flight_duration_max = 25
